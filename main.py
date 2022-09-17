e5ab8f8bda60f6287ca340d8a708b7dae1631804
Orlando-Schneider-Moser <113249812+Orlando-Schneider-Moser@users.noreply.github.com>
2022-09-17 14:12:01 +0200

    a

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ pygame.display.set_caption('Hello World!')
 
 
 #==================================================
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            pass
+            if event.key == pygame.K_escape:
+                break
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
+
+pygame.quit()
+sys.exit()
+
